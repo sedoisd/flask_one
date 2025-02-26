77d473b1eed491dc9381b10fefc9f51262a08acc
sedoisd <saraskin.serafim@yandex.ru>
2025-02-26 11:51:19 +0400

    adver with image

--- un.py
+++ un.py
@@ -47,5 +47,43 @@ def image_mars():
                     </html>'''
 
 
+@app.route('/promotion_image')
+def promotion_image():
+    return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static',
+                                                                                  filename='css/style.css')}" />
+                            <link rel="stylesheet" 
+                                href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                                integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                            crossorigin="anonymous">
+                            
+                            <title>Привет, Марс</title>
+                          </head>
+                          <body>
+                            <h1>Жди нас, Марс!</h1>
+                            <img src="{url_for('static', filename='img/mars.png')}"
+                               alt="здесь должна была быть картинка, но не нашлась">
+                            <div class="alert alert-primary" style="background-color:grey">
+                                Человечество вырастает из детства.
+                            </div>
+                            <div class="alert alert-primary" style="background-color:green">
+                                Человечеству мала одна планета.
+                            </div>
+                            <div class="alert alert-primary" style="background-color:blue">
+                                Мы сделаем обитаемыми безжизненные пока планеты.
+                            </div>
+                            <div class="alert alert-primary" style="background-color:yellow">
+                                И начнем с Марса!
+                            </div>
+                            <div class="alert alert-primary" style="background-color:red">
+                                Присоединяйся!
+                            </div>
+                          </body>
+                        </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
